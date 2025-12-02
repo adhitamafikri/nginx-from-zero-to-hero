7ae5e7b9de94a543d4e2eb10e576355fd104dfb6
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2025-12-02 07:37:22 +0700

    feat(gzip): evidence script

--- 00-baby-steps/05-gzip/evidence.sh
+++ 00-baby-steps/05-gzip/evidence.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-request_spa_js() {
+request_gzipped() {
     # Change the content hash if you build the project again
     url="http://nginx-zero-to-hero.local:8825/assets/index-CzHpBxx8.js"
-    curl -I -H "Accept-Encoding: gzip" $url > evidence.txt
+    result=$(curl -H "Accept-Encoding: gzip" -s -o /dev/null -w "%{size_download}" $url)
+    echo "GZIPPED: $result" >> evidence.txt
     echo "--------------------------------"
     cat evidence.txt
     echo "--------------------------------"
 }
 
-request_spa_js
+request_not_gzipped() {
+    # Change the content hash if you build the project again
+    url="http://nginx-zero-to-hero.local:8825/assets/index-CzHpBxx8.js"
+    result=$(curl -H "Accept-Encoding: identity" -s -o /dev/null -w "%{size_download}" $url)
+    echo "NOT GZIPPED: $result" >> evidence.txt
+    echo "--------------------------------"
+    cat evidence.txt
+    echo "--------------------------------"
+}
+
+cat /dev/null > evidence.txt
+echo "Running evidence script:"
+request_gzipped
+request_not_gzipped
